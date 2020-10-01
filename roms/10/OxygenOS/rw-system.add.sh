50d1fd39d8164a6ab9cf9e9ba589c6133c1815f8
henloboi <henloboii@protonmail.ch>
2020-10-01 10:41:07 +0800

    [TEST] rosy : auto 1080p on boot

--- roms/10/OxygenOS/rw-system.add.sh
+++ roms/10/OxygenOS/rw-system.add.sh
@@ -2,3 +2,8 @@ if [ "$vndk" == 28 ]; then
     mount -o bind /system/bin/wpa_supplicant /vendor/bin/hw/wpa_supplicant || true
 fi
 mount -o bind /mnt/phh/empty_dir /vendor/overlay || true
+
+if [[ $(cat /vendor/build.prop | grep rosy) ]]; then
+    wm size 1080x2160
+    wm density 480
+fi
